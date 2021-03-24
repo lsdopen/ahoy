b54f6ef4c991b7f09eaf2128a57d137a9f130ce5
Clive Borrageiro <clive@lsd.co.za>
2021-03-24 16:54:35 +0200

    Make Enter the default confirm key for dialogs

--- ahoy-ui/src/app/components/dialog.service.ts
+++ ahoy-ui/src/app/components/dialog.service.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright  2020 LSD Information Technology (Pty) Ltd
+ * Copyright  2021 LSD Information Technology (Pty) Ltd
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -42,6 +42,11 @@ export class DialogService {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.data = description;
 
-    this.dialog.open(DescriptionDialogComponent, dialogConfig);
+    const dialogRef = this.dialog.open(DescriptionDialogComponent, dialogConfig);
+    dialogRef.keydownEvents().subscribe(event => {
+      if (event.key === 'Enter') {
+        dialogRef.close();
+      }
+    });
   }
 }
