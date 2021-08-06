4052219cca74ccbd2c0505d10d0116d3ca595a6e
Clive Borrageiro <clive@lsd.co.za>
2021-08-06 17:02:39 +0200

    PrimeNG template for UI - footer social links to LSD Open - removed unnecessary nav components

--- ahoy-ui/src/app/app.footer.component.ts
+++ ahoy-ui/src/app/app.footer.component.ts
@@ -18,16 +18,23 @@ import {Component} from '@angular/core';
 import {AppComponent} from './app.component';
 
 @Component({
-    selector: 'app-footer',
-    template: `
-        <div class="layout-footer p-d-flex p-ai-center p-p-4 p-shadow-2">
-            <img id="footer-logo" [src]="'assets/layout/images/footer-' + (app.layoutMode === 'light' ? 'ultima' : 'ultima-dark') + '.svg'" alt="ultima-footer-logo">
-            <button pButton pRipple type="button" icon="pi pi-github fs-large" class="p-button-rounded p-button-text p-button-plain" [ngClass]="{'p-ml-auto p-mr-2': !app.isRTL, 'p-ml-2 p-mr-auto': app.isRTL}"></button>
-            <button pButton pRipple type="button" icon="pi pi-facebook fs-large" class="p-button-rounded p-button-text p-button-plain" [ngClass]="{'p-mr-2': !app.isRTL, 'p-ml-2': app.isRTL}"></button>
-            <button pButton pRipple type="button" icon="pi pi-twitter fs-large" class="p-button-rounded p-button-text p-button-plain" [ngClass]="{'p-mr-2': !app.isRTL, 'p-ml-2': app.isRTL}"></button>
-        </div>
-    `
+  selector: 'app-footer',
+  template: `
+    <div class="layout-footer p-d-flex p-ai-center p-p-4 p-shadow-2">
+      <img id="footer-logo" [src]="'assets/layout/images/footer-' + (app.layoutMode === 'light' ? 'ultima' : 'ultima-dark') + '.svg'" alt="ultima-footer-logo">
+      <a pButton pRipple icon="pi pi-github fs-large" class="p-button-rounded p-button-text p-button-plain"
+         href="https://github.com/lsdopen" target="_blank"
+         [ngClass]="{'p-ml-auto p-mr-2': !app.isRTL, 'p-ml-2 p-mr-auto': app.isRTL}"></a>
+      <a pButton pRipple type="button" icon="pi pi-facebook fs-large" class="p-button-rounded p-button-text p-button-plain"
+         href="https://www.facebook.com/lsdopen" target="_blank"
+         [ngClass]="{'p-mr-2': !app.isRTL, 'p-ml-2': app.isRTL}"></a>
+      <a pButton pRipple type="button" icon="pi pi-twitter fs-large" class="p-button-rounded p-button-text p-button-plain"
+         href="https://twitter.com/lsdopen" target="_blank"
+         [ngClass]="{'p-mr-2': !app.isRTL, 'p-ml-2': app.isRTL}"></a>
+    </div>
+  `
 })
 export class AppFooterComponent {
-    constructor(public app: AppComponent) {}
+  constructor(public app: AppComponent) {
+  }
 }
