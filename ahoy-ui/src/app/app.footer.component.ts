fd157767e18c9d74c9348ab10e42ef914488b3c7
Clive Borrageiro <clive@lsd.co.za>
2021-08-25 15:59:53 +0200

    PrimeNG template for UI - added version to footer

--- ahoy-ui/src/app/app.footer.component.ts
+++ ahoy-ui/src/app/app.footer.component.ts
@@ -15,6 +15,7 @@
  */
 
 import {Component} from '@angular/core';
+import {version} from '../../package.json';
 import {AppComponent} from './app.component';
 
 @Component({
@@ -22,9 +23,10 @@ import {AppComponent} from './app.component';
   template: `
     <div class="layout-footer p-d-flex p-ai-center p-p-4 p-shadow-2">
       <img id="footer-logo" [src]="'assets/layout/images/footer-' + (app.layoutMode === 'light' ? 'dark' : 'light') + '.png'" alt="ultima-footer-logo" style="height: 2.25rem">
+      <span [ngClass]="{'p-ml-auto p-mr-2': !app.isRTL, 'p-ml-2 p-mr-auto': app.isRTL}">{{version}}</span>
       <a pButton pRipple icon="pi pi-github fs-large" class="p-button-rounded p-button-text p-button-plain"
          href="https://github.com/lsdopen" target="_blank"
-         [ngClass]="{'p-ml-auto p-mr-2': !app.isRTL, 'p-ml-2 p-mr-auto': app.isRTL}"></a>
+         [ngClass]="{'p-mr-2': !app.isRTL, 'p-ml-2': app.isRTL}"></a>
       <a pButton pRipple type="button" icon="pi pi-facebook fs-large" class="p-button-rounded p-button-text p-button-plain"
          href="https://www.facebook.com/lsdopen" target="_blank"
          [ngClass]="{'p-mr-2': !app.isRTL, 'p-ml-2': app.isRTL}"></a>
@@ -35,6 +37,8 @@ import {AppComponent} from './app.component';
   `
 })
 export class AppFooterComponent {
+  public version: string = version;
+
   constructor(public app: AppComponent) {
   }
 }
