0d1e76f6ef5aee80d63575bc0a8b5e5f25be978e
Clive Borrageiro <clive@lsd.co.za>
2022-03-31 12:11:33 +0200

    Split community components library and UI - fixed build/test failure

--- ahoy-ui/projects/ahoy-components/src/test.ts
+++ ahoy-ui/projects/ahoy-components/src/test.ts
@@ -16,10 +16,10 @@
 
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import {getTestBed} from '@angular/core/testing';
-import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
 import 'zone.js';
 import 'zone.js/testing';
+import {getTestBed} from '@angular/core/testing';
+import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
 
 declare const require: {
   context(path: string, deep?: boolean, filter?: RegExp): {
